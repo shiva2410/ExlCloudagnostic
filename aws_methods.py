49ccf0445930b5e482d9d9157d580e00011a64a1
Shiva Thavani <shiva@Shivas-MacBook-Air.local>
2022-07-05 23:47:37 +0530

    Download using temporary link functionality added

--- aws_methods.py
+++ aws_methods.py
@@ -1,6 +1,8 @@
 from boto3.session import Session
 import boto3
 import os
+import pyshorteners
+type_tiny = pyshorteners.Shortener()
 
 # ACCESS_KEY = 'AKIAY6KCSSCHL63PJUEF'
 # SECRET_KEY = 'JI9/i4jdBa764P4xGM6ZVAv0CyLFO6OAiXM8A6m'
@@ -8,7 +10,7 @@ import os
 # ACCESS_KEY = 'AKIAY6KCSSCHCRJ7BGO4'
 # SECRET_KEY = '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'
 
-def download_file_aws(bucket_name,filename):
+def download_file_aws(bucket_name,filename):     #Function to Download files from AWS Bucket
 	session = Session(aws_access_key_id=ACCESS_KEY,
 	              aws_secret_access_key=SECRET_KEY)
 	s3 = session.resource('s3')
@@ -18,12 +20,12 @@ def download_file_aws(bucket_name,filename):
 	your_bucket.download_file(filename,filename)
 	return os.path.join(os.getcwd(),filename)
 
-def upload_file_aws(bucket_name,filename):
+def upload_file_aws(bucket_name,filename):      #Function to Upload file to AWS Bucket
 	try:
 		session = Session(aws_access_key_id=ACCESS_KEY,
 	              aws_secret_access_key=SECRET_KEY)
 		s3 = session.resource('s3')
-		Filename='kpmg.gif'
+		Filename='kpmg.gif'     
 		Bucket='exlhackathon'
 		Key="/uploads/" 
 		s3.meta.client.upload_file(Filename=Filename, Bucket=Bucket, Key=Filename)
@@ -42,8 +44,20 @@ def list_items_aws_bucket(bucket_name): # prints the contents of bucket
 	    items.append(s3_file.key)
 	return items
 
+def download_temp_access(bucket_name,filename,expiration_time):  #Function to get temporary file download link
+	s3_client = boto3.client('s3')
+	long_url = s3_client.generate_presigned_url('get_object',Params={'Bucket': bucket_name, 'Key': filename},ExpiresIn=expiration_time)
+	try:
+		short_url = type_tiny.tinyurl.short(long_url)
+		return short_url
+	except:
+		return long_url
+
+
 filename='kpmg.gif'
 bucket_name='exlhackathon'
-print(download_file_aws(bucket_name,filename))
-print(upload_file_aws(bucket_name,filename))
-print(list_items_aws_bucket(bucket_name))
+expiration_time=60
+# print(download_file_aws(bucket_name,filename))
+# print(upload_file_aws(bucket_name,filename))
+# print(list_items_aws_bucket(bucket_name))
+print(download_temp_access(bucket_name,filename,expiration_time))
